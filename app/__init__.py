33a63de3c1a8b60c758123fc487c8143047bdbb1
umluizlima <umluizlima@gmail.com>
2018-08-07 12:37:38 -0300

    Flask Conf first commit.

--- app/__init__.py
+++ app/__init__.py
@@ -3,25 +3,18 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__, instance_relative_config=True, static_url_path='')
-    app.config.from_mapping(
-        SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess',
-        # SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(app.instance_path, 'app.db'),
-        # SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    app = Flask(
+        __name__,
+        instance_relative_config=True,
+        static_url_path=''
     )
-
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    )
 
     from flask_sslify import SSLify
     if 'DYNO' in os.environ:  # only trigger SSLify if the app is running on Heroku
         sslify = SSLify(app)
-
-    # from app.model import db, migrate
-    # db.init_app(app)
-    # migrate.init_app(app, db)
 
     from app.controller import main
     app.register_blueprint(main.bp)
--- app/controller/main.py
+++ app/controller/main.py
@@ -5,7 +5,66 @@ from flask import (
 bp = Blueprint('main', __name__)
 
 
+atividades = [
+    {
+        'title': 'Credenciamento',
+        'time': '08:00'
+    },
+    {
+        'title': 'Usando o Flask Admin com segurança e flexibilidade',
+        'time': '09:10',
+        'speaker': 'Thaíssa Falbo'
+    },
+    {
+        'title': 'Escrevendo Testes com Flask e #JustPython',
+        'time': '08:30',
+        'speaker': 'Eduardo Mendes'
+    },
+    {
+        'title': 'Inteligência Artificial, Flask e Raspberry Pi',
+        'time': '11:00',
+        'speaker': 'Eduardo Pereira'
+    },
+    {
+        'title': 'API REST o começo: Introdução a REST para iniciantes',
+        'time': '15:20',
+        'speaker': 'Talita Rossari'
+    },
+    {
+        'title': 'Flask Profiling: Técnicas para identificação de \
+pontos críticos de performance',
+        'time': '13:00',
+        'speaker': 'Iuri de Silvio'
+    },
+    {
+        'title': 'API REST em Flask + Vue.js: Aplicações modernas \
+com separação entre frontend e backend',
+        'time': '14:00',
+        'speaker': 'João Lugão'
+    },
+]
+
+
 @bp.route('/')
 def index():
-    return render_template('main/index.html',
-                           title='Flask-PWA')
+    return render_template(
+        'main/index.html',
+        title='Flask Conf 2018'
+    )
+
+
+@bp.route('/agenda')
+def agenda():
+    return render_template(
+        'main/agenda.html',
+        title='Agenda',
+        agenda=sorted(atividades, key=lambda k: k['time'])
+    )
+
+
+@bp.route('/cdc')
+def cdc():
+    return render_template(
+        'main/cdc.html',
+        title='Código de conduta'
+    )
